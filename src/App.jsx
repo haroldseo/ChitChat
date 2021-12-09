3c04db6dcd4984f9ce142e311a83003221671813
Harold Seo <harold.seo.612@gmail.com>
2021-12-08 21:40:59 -0800

    Add Auth Component (Sign Up and Sign In Form)

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 
-import { ChannelContainer, ChannelListContainer } from "./components";
+import { ChannelContainer, ChannelListContainer, Auth } from "./components";
 
 import "./App.css";
 
 const client = StreamChat.getInstance(process.env.REACT_APP_STREAM_API_KEY);
 
+const authToken = false;
+
 const App = () => {
+  if (!authToken) return <Auth />;
+
   return (
     <div className='app__wrapper'>
       <Chat client={client}>
